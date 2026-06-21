fd5ca9e75798393e7ade8c6b6b319487865cb1b6
manik-babu <mdmanikbabu48@gmail.com>
2026-06-21 19:09:26 +0600

    feat: jwt token validate

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -19,7 +20,7 @@ type JWTClaims struct {
 
 type JWTService interface {
 	GenerateToken(id uint, email string) (string, error)
-	// ValidateToken(jwtToken string) (*JWTClaims, error)
+	ValidateToken(jwtToken string) (*JWTClaims, error)
 }
 
 type jwtService struct {
@@ -54,7 +55,18 @@ func (s *jwtService) GenerateToken(id uint, email string) (string, error) {
 	}
 	return tokenStr, nil
 }
-
-// func (s * jwtService) ValidateToken(token string) (*JWTClaims, error){
-
-// }
+func (s *jwtService) ValidateToken(tokenStr string) (*JWTClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenStr, JWTClaims{}, func(token *jwt.Token) (any, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Invalid singing method: %v", token.Header["alg"])
+		}
+		return []byte(s.secretKey), nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("Invalid singing method: %v", token.Header["alg"])
+	}
+	if claims, ok := token.Claims.(*JWTClaims); ok && token.Valid {
+		return claims, nil
+	}
+	return nil, fmt.Errorf("Invalid token")
+}
