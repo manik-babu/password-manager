5630d1ac4d3d3d86ba6c7e7f9825f92a0bc1ee15
manik-babu <mdmanikbabu48@gmail.com>
2026-06-21 21:35:04 +0600

    feat: auth middleware

---
+++ internal/middleware/auth.go
@@ -0,0 +1,46 @@
+package middleware
+
+import (
+	"detrox/internal/auth"
+	"detrox/internal/httpresponse"
+	"net/http"
+	"strings"
+
+	"github.com/labstack/echo/v5"
+)
+
+func AuthMiddleWare(jwtService auth.JWTService) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c *echo.Context) error {
+			authHeader := c.Request().Header.Get("Authorization")
+			if authHeader == "" {
+				return c.JSON(http.StatusUnauthorized, httpresponse.Error{
+					Ok:      false,
+					Code:    401,
+					Message: "No token provided",
+				})
+			}
+			parts := strings.Split(authHeader, " ")
+			if len(parts) != 2 || parts[0] != "bearer" {
+				return c.JSON(401, httpresponse.Error{
+					Ok:      false,
+					Code:    401,
+					Message: "No token provided",
+				})
+			}
+			tokenStr := parts[1]
+			claims, err := jwtService.ValidateToken(tokenStr)
+
+			if err != nil {
+				return c.JSON(401, httpresponse.Error{
+					Ok:      false,
+					Code:    401,
+					Message: err.Error(),
+				})
+			}
+			c.Set("user", claims)
+
+			return next(c)
+		}
+	}
+}
